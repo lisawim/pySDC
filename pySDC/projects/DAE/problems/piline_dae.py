4489ac42eb3d52e35ff2cf7f012261851deb6e48
lisawim <wimmer@uni-wuppertal.de>
2023-06-14 18:22:19 +0200

    Inheritance for Piline!

--- pySDC/projects/DAE/problems/piline_dae.py
+++ pySDC/projects/DAE/problems/piline_dae.py
@@ -6,7 +6,7 @@ from pySDC.projects.DAE.misc.ProblemDAE import ptype_dae
 from pySDC.implementations.datatype_classes.mesh import mesh
 
 
-class Piline_DAE(ptype_dae):
+class PilineDAE(ptype_dae):
     r"""
     This class implements the piline from the PinTSimE project modelled as system of differential-algebraic
     equations (DAEs). It serves as a transmission line in an energy grid. The voltage source :math:`V_s` is a
@@ -77,19 +77,8 @@ class Piline_DAE(ptype_dae):
         Value between 0 and 1. Indicates when the backup generator must restart.
     newton_tol : float, optional
         Tolerance of the Newton-like solver.
-
-    Attributes
-    ----------
-    nvars: int
+    nvars : int
         Number of unknowns in the DAE system.
-    power_outage: bool
-        Indicates if the power outage already takes place or not.
-    t_outage: float
-        Time from when the power outage occurs.
-    t_switch: float
-        Time point of the discrete event found by switch estimation.
-    nswitches: int
-        Number of switches found by switch estimation.
 
     Note
     ----
@@ -120,6 +109,124 @@ class Piline_DAE(ptype_dae):
         )
 
         assert 0 < self.c < 1, 'c needs to be between 0 and 1!'
+
+    def eval_f(self, u, du, t):
+        r"""
+        Routine to evaluate the implicit representation of the problem, i.e., :math:`F(u, u', t)`.
+
+        Parameters
+        ----------
+        u : dtype_u
+            Current values of the numerical solution at time t.
+        du : dtype_u
+            Current values of the derivative of the numerical solution at time t.
+        t : float
+            Current time of the numerical solution.
+
+        Returns
+        -------
+        f : dtype_f
+            Current value of the right-hand side of f (which includes 12 components).
+        """
+
+        vRs, vC1, vRp, vLp, vC2, vRl = u[0], u[1], u[2], u[3], u[4], u[5]
+        iRs, iC1, iRp, iLp, iC2, iRl = u[6], u[7], u[8], u[9], u[10], u[11]
+
+        dvC1, dvC2, diLp = du[1], du[4], du[9]
+
+        f = self.dtype_f(self.init)
+        f[:] = (
+            dvC1 - iC1 / self.C1,
+            dvC2 - iC2 / self.C2,
+            diLp - vLp / self.Lp,
+            vRs - self.Rs * iRs,
+            vRp - self.Rp * iRp,
+            vRl - self.Rl * iRl,
+            vC1 - vRp - vLp - vC2,
+            self.Vs - vRs - vC1,
+            vC2 - vRl,
+            iRs - iC1 - iRp,
+            iRp - iLp,
+            iLp - iC2 - iRl,
+        )
+        return f
+
+    def u_exact(self, t):
+        """
+        Routine for the exact solution.
+
+        Parameters
+        ----------
+        t : float
+            The time of the reference solution.
+
+        Returns
+        -------
+        me : dtype_u
+            The reference solution as mesh object containing 12 components.
+        """
+
+        assert t == 0, 'ERROR: u_exact only valid for t=0'
+
+        me = self.dtype_u(self.init)
+        me[0] = 0.0  # vRs
+        me[1] = 0.0  # vC1
+        me[2] = 0.0  # vRp
+        me[3] = 0.0  # vLp
+        me[4] = 0.0  # vC2
+        me[5] = 0.0  # vRl
+        me[6] = 0.0  # iRs
+        me[7] = 0.0  # iC1
+        me[8] = 0.0  # iRp
+        me[9] = 0.0  # iLp
+        me[10] = 0.0  # iC2
+        me[11] = 0.0  # iRl
+
+        return me
+
+
+class PilinePowerOutageDAE(PilineDAE):
+    """
+    This class inherits from the PilineDAE class which a system of differential-algebraic equations (DAEs) without
+    any discrete event. Here, an event takes place: The voltage source :math:`V_s` is a
+    generator that provides energy. Hence, the consumer modelled as resistor :math:`R_\ell` is always supplied
+    with :math:`c V_s` with :math:`0 < c < 1`. At time :math:`t_{outage}` there is a power outage (and :math:`V_s`
+    will be set to zero). When the energy :math:`V_{R_\ell}` drops below :math:`c V_s`, i.e., when the
+    state function :math:`h(V_{R_\ell}) := V_{R_\ell} - c V_s` changes from positive to negative, a backup
+    generator starts to continue to supply the consumer with energy, and :math:`V_s` will be set back to its
+    old value. The DAE system can be found in the parent class.
+
+    Attributes
+    ----------
+    power_outage: bool
+        Indicates if the power outage already takes place or not.
+    t_outage: float
+        Time from when the power outage occurs.
+    t_switch: float
+        Time point of the discrete event found by switch estimation.
+    nswitches: int
+        Number of switches found by switch estimation.
+    """
+
+    def __init__(self, Vs=150.0, Rs=1.0, C1=1e-2, Rp=0.2, Lp=1e-2, C2=1e-2, Rl=0.5, c=0.2, nvars=12, newton_tol=1e-12):
+        """Initialization routine"""
+
+        # invoke super init, passing number of dofs
+        super().__init__(nvars, newton_tol)
+        self._makeAttributeAndRegister(
+            'Vs',
+            'Rs',
+            'C1',
+            'Rp',
+            'Lp',
+            'C2',
+            'Rl',
+            'c',
+            'nvars',
+            'newton_tol',
+            localVars=locals(),
+            readOnly=True,
+        )
 
         self.power_outage = False
         self.t_outage = 0.05
@@ -178,42 +285,6 @@ class Piline_DAE(ptype_dae):
             iLp - iC2 - iRl,
         )
         return f
-
-    def u_exact(self, t):
-        """
-        Routine for the exact solution.
-
-        Parameters
-        ----------
-        t : float
-            The time of the reference solution.
-
-        Returns
-        -------
-        me : dtype_u
-            The reference solution as mesh object containing 12 components.
-        """
-
-        assert t == 0, 'ERROR: u_exact only valid for t=0'
-
-        me = self.dtype_u(self.init)
-        me[0] = 0.0  # vRs
-        me[1] = 0.0  # vC1
-        me[2] = 0.0  # vRp
-        me[3] = 0.0  # vLp
-        me[4] = 0.0  # vC2
-        me[5] = 0.0  # vRl
-        me[6] = 0.0  # iRs
-        me[7] = 0.0  # iC1
-        me[8] = 0.0  # iRp
-        me[9] = 0.0  # iLp
-        me[10] = 0.0  # iC2
-        me[11] = 0.0  # iRl
-
-        #if me[5] < self.c * self.Vs:
-        #    raise ParameterError(f"vRl has to be initialized greater than {self.c*self.Vs}!")
-
-        return me
 
     def get_switching_info(self, u, t):
         """

0e6a5284cd0d350d69082298a6ec4996038ad998
lisawim <wimmer@uni-wuppertal.de>
2023-06-26 16:02:27 +0200

    Inherently improvement of SE and better switching in the problem test class

--- pySDC/projects/DAE/problems/test_DAE.py
+++ pySDC/projects/DAE/problems/test_DAE.py
@@ -9,7 +9,7 @@ class ScalarTestDAE(ptype_dae):
     This class implements a scalar test discontinuous differential-algebraic equation (DAE) [1]_. The event function
     is defined as :math:`h(y, z):= 2yz - 100`. Then, the discontinuous DAE model reads:
 
-    - if :math:`h(y, z) < 0`:
+    - if :math:`h(y, z) \leq 0`:
 
         .. math::
             \dfrac{d y(t)}{dt} = z(t),
@@ -88,19 +88,24 @@ class ScalarTestDAE(ptype_dae):
         dy = du[0]
 
         t_switch = np.inf if self.t_switch is None else self.t_switch
-        h = 2 * y * z - 100
 
+        h = 2 * y * z - 100
         f = self.dtype_f(self.init)
-        if h >= 0 or t >= t_switch:
-            f[:] = (
-                dy,
-                y**2 - z**2 - 1,
-            )
+
+        f_before_event = (
+            dy - z,
+            y**2 - z**2 - 1,
+        )
+
+        f_after_event = (
+            dy,
+            y**2 - z**2 - 1,
+        )
+
+        if self.t_switch is not None:
+            f[:] = f_before_event if t <= self.t_switch else f_after_event
         else:
-            f[:] = (
-                dy - z,
-                y**2 - z**2 - 1,
-            )
+            f[:] = f_before_event if h <= 0 else f_after_event
         return f
 
     def u_exact(self, t):
@@ -121,7 +126,7 @@ class ScalarTestDAE(ptype_dae):
         assert t >= 1, 'ERROR: u_exact only valid for t>=1'
 
         me = self.dtype_u(self.init)
-        if t < self.t_switch_exact:
+        if t <= self.t_switch_exact:
             me[:] = (np.cosh(t), np.sinh(t))
         else:
             me[:] = (np.cosh(self.t_switch_exact), np.sinh(self.t_switch_exact))
@@ -130,7 +135,9 @@ class ScalarTestDAE(ptype_dae):
     def get_switching_info(self, u, t):
         """
         Provides information about the state function of the problem. When the state function changes its sign,
-        typically an event occurs.
+        typically an event occurs. So the check for an event should be done in the way that the state function
+        is checked for a sign change. If this is the case, the intermediate value theorem states a root in this
+        step.
 
         Parameters
         ----------
@@ -152,8 +159,10 @@ class ScalarTestDAE(ptype_dae):
         switch_detected = False
         m_guess = -100
 
-        for m in range(len(u)):
-            if 2 * u[m][0] * u[m][1] - 100 >= 0:
+        for m in range(1, len(u)):
+            h_prev_node = 2 * u[m - 1][0] * u[m - 1][1] - 100
+            h_curr_node = 2 * u[m][0] * u[m][1] - 100
+            if h_prev_node <= 0 and h_curr_node > 0:
                 switch_detected = True
                 m_guess = m - 1
                 break
@@ -167,3 +176,4 @@ class ScalarTestDAE(ptype_dae):
         Setter to update the number of switches if one is found.
         """
         self.nswitches += 1
+
--- pySDC/projects/PinTSimE/switch_estimator.py
+++ pySDC/projects/PinTSimE/switch_estimator.py
@@ -3,26 +3,32 @@ import scipy as sp
 
 from pySDC.core.Collocation import CollBase
 from pySDC.core.ConvergenceController import ConvergenceController, Status
+from pySDC.implementations.convergence_controller_classes.check_convergence import CheckConvergence
 
 
 class SwitchEstimator(ConvergenceController):
     """
-    Class to predict the time point of the switch and setting a new step size
-
-    For the first time, this is a nonMPI version, because a MPI version is not yet developed.
+    Class to predict the time point of the switch and setting a new step size. For the first time, this is a
+    nonMPI version, because a MPI version is not yet developed.
     """
 
     def setup(self, controller, params, description):
         """
         Function sets default variables to handle with the switch at the beginning.
 
-        Args:
-            controller (pySDC.Controller): The controller
-            params (dict): The params passed for this specific convergence controller
-            description (dict): The description object used to instantiate the controller
+        Parameters
+        ----------
+        controller : pySDC.Controller
+            The controller doing all the stuff in a computation.
+        params : dict
+            The parameters passed for this specific convergence controller.
+        description : dict
+            The description object used to instantiate the controller.
 
-        Returns:
-            (dict): The updated params dictionary
+        Returns
+        -------
+        convergence_controller_params : dict
+            The updated params dictionary.
         """
 
         # for RK4 sweeper, sweep.coll.nodes now consists of values of ButcherTableau
@@ -34,27 +40,32 @@ class SwitchEstimator(ConvergenceController):
 
         defaults = {
             'control_order': 100,
-            'tol': description['level_params']['dt'],
+            'tol': 1e-11,
             'nodes': coll.nodes,
         }
-        return {**defaults, **params}
+        convergence_controller_params = {**defaults, **params}
+        return convergence_controller_params
 
     def setup_status_variables(self, controller, **kwargs):
         """
         Adds switching specific variables to status variables.
 
-        Args:
-            controller (pySDC.Controller): The controller
+        Parameters
+        ----------
+        controller : pySDC.Controller
+            The controller doing all the stuff in a computation.
         """
 
-        self.status = Status(['switch_detected', 't_switch'])
+        self.status = Status(['is_zero', 'switch_detected', 't_switch'])
 
     def reset_status_variables(self, controller, **kwargs):
         """
         Resets status variables.
 
-        Args:
-            controller (pySDC.Controller): The controller
+        Parameters
+        ----------
+        controller : pySDC.Controller
+            The controller doing all the stuff in a computation.
         """
 
         self.setup_status_variables(controller, **kwargs)
@@ -63,39 +74,40 @@ class SwitchEstimator(ConvergenceController):
         """
         Determine a new step size when a switch is found such that the switch happens at the time step.
 
-        Args:
-            controller (pySDC.Controller): The controller
-            S (pySDC.Step): The current step
-
-        Returns:
-            None
+        Parameters
+        ----------
+        controller : pySDC.Controller
+            The controller doing all the stuff in a computation.
+        S : pySDC.Step
+            The current step.
         """
 
         L = S.levels[0]
 
-        if S.status.iter == S.params.maxiter:
-            self.status.switch_detected, m_guess, vC_switch = L.prob.get_switching_info(L.u, L.time)
+        if CheckConvergence.check_convergence(S):
+            self.status.switch_detected, m_guess, state_function = L.prob.get_switching_info(L.u, L.time)
 
             if self.status.switch_detected:
                 t_interp = [L.time + L.dt * self.params.nodes[m] for m in range(len(self.params.nodes))]
+                t_interp, state_function = self.adapt_interpolation_info(
+                    L.time, L.sweep.coll.left_is_node, t_interp, state_function
+                )
 
-                # only find root if vc_switch[0], vC_switch[-1] have opposite signs (intermediate value theorem)
-                if vC_switch[0] * vC_switch[-1] < 0:
-                    self.status.t_switch = self.get_switch(t_interp, vC_switch, m_guess)
+                # when the state function is already close to zero the event is already resolved well
+                if all(np.isclose(state_function[0], np.zeros(len(t_interp)), atol=1e-11)):
+                   self.status.is_zero = True
 
-                    if L.time <= self.status.t_switch <= L.time + L.dt:
+                # intermediate value theorem states that a root is contained in current step
+                if state_function[0] * state_function[-1] < 0 and self.status.is_zero is None:
+                    self.status.t_switch = self.get_switch(t_interp, state_function, m_guess)
+
+                    if L.time < self.status.t_switch < L.time + L.dt:
                         dt_switch = self.status.t_switch - L.time
-                        if not np.isclose(self.status.t_switch - L.time, L.dt, atol=self.params.tol):
-                            self.log(
-                                f"Located Switch at time {self.status.t_switch:.6f} is outside the range of tol={self.params.tol:.4e}",
-                                S,
-                            )
 
-                        else:
-                            self.log(
-                                f"Switch located at time {self.status.t_switch:.6f} inside tol={self.params.tol:.4e}", S
-                            )
-
+                        if np.isclose(L.time, self.status.t_switch, atol=self.params.tol) or np.isclose(
+                            L.time + L.dt, self.status.t_switch, atol=self.params.tol
+                        ):
+                            self.log(f"Switch located at time {self.status.t_switch:.12f}", S)
                             L.prob.t_switch = self.status.t_switch
                             controller.hooks[0].add_to_stats(
                                 process=S.status.slot,
@@ -109,31 +121,33 @@ class SwitchEstimator(ConvergenceController):
 
                             L.prob.count_switches()
 
-                        dt_planned = L.status.dt_new if L.status.dt_new is not None else L.params.dt
+                        else:
+                            self.log(f"Located Switch at time {self.status.t_switch:.12f} is outside the range", S)
 
-                        # when a switch is found, time step to match with switch should be preferred
+                        # when an event is found, step size matching with this event should be preferred
+                        dt_planned = L.status.dt_new if L.status.dt_new is not None else L.params.dt
                         if self.status.switch_detected:
                             L.status.dt_new = dt_switch
-
                         else:
                             L.status.dt_new = min([dt_planned, dt_switch])
 
                     else:
+                        # event occurs on L.time or L.time + L.dt; no restart necessary
                         self.status.switch_detected = False
 
-                else:
+                else:  # intermediate value theorem is not satisfied
                     self.status.switch_detected = False
 
     def determine_restart(self, controller, S, **kwargs):
         """
         Check if the step needs to be restarted due to a predicting switch.
 
-        Args:
-            controller (pySDC.Controller): The controller
-            S (pySDC.Step): The current step
-
-        Returns:
-            None
+        Parameters
+        ----------
+        controller : pySDC.Controller
+            The controller doing all the stuff in a computation.
+        S : pySDC.Step
+            The current step.
         """
 
         if self.status.switch_detected:
@@ -147,44 +161,87 @@ class SwitchEstimator(ConvergenceController):
         After a step is done, some variables will be prepared for predicting a possibly new switch.
         If no Adaptivity is used, the next time step will be set as the default one from the front end.
 
-        Args:
-            controller (pySDC.Controller): The controller
-            S (pySDC.Step): The current step
-
-        Returns:
-            None
+        Parameters
+        ----------
+        controller : pySDC.Controller
+            The controller doing all the stuff in a computation.
+        S : pySDC.Step
+            The current step.
         """
 
         L = S.levels[0]
 
         if self.status.t_switch is None:
             L.status.dt_new = L.status.dt_new if L.status.dt_new is not None else L.params.dt_initial
+            L.prob.t_switch = None
 
         super().post_step_processing(controller, S, **kwargs)
 
     @staticmethod
-    def get_switch(t_interp, vC_switch, m_guess):
+    def get_switch(t_interp, state_function, m_guess):
         """
         Routine to do the interpolation and root finding stuff.
 
-        Args:
-            t_interp (list): collocation nodes in a step
-            vC_switch (list): differences vC - V_ref at these collocation nodes
-            m_guess (np.float): Index at which the difference drops below zero
+        Parameters
+        ----------
+        t_interp : list
+            Collocation nodes in a step.
+        state_function : list
+            Contains values of state function at these collocation nodes.
+        m_guess : float
+            Index at which the difference drops below zero.
 
-        Returns:
-            t_switch (np.float): time point of th switch
+        Returns
+        -------
+        t_switch : float
+           Time point of the founded switch.
         """
 
-        p = sp.interpolate.interp1d(t_interp, vC_switch, 'cubic', bounds_error=False)
+        kind = 'quadratic' if len(t_interp) <= 3 else 'cubic'
+        p = sp.interpolate.interp1d(t_interp, state_function, kind=kind, bounds_error=False)
 
         SwitchResults = sp.optimize.root_scalar(
             p,
             method='brentq',
-            bracket=[t_interp[0], t_interp[m_guess]],
+            bracket=[t_interp[0], t_interp[-1]],
             x0=t_interp[m_guess],
-            xtol=1e-10,
+            xtol=1e-14,
         )
         t_switch = SwitchResults.root
 
         return t_switch
+
+    @staticmethod
+    def adapt_interpolation_info(t, left_is_node, t_interp, state_function):
+        """
+        Adapts the x- and y-axis for interpolation. For SDC, it is proven whether the left boundary is a
+        collocation node or not. In case it is, the first entry of the state function has to be removed,
+        because it would otherwise contain double values on starting time and the first node. Otherwise,
+        starting time L.time has to be added to t_interp to also take this value in the interpolation
+        into account.
+
+        Parameters
+        ----------
+        t : float
+            Starting time of the step.
+        left_is_node : bool
+            Indicates whether the left boundary is a collocation node or not.
+        t_interp : list
+            x-values for interpolation containing collocation nodes.
+        state_function : list
+            y-values for interpolation containing values of state function.
+
+        Returns
+        -------
+        t_interp : list
+            Adapted x-values for interpolation containing collocation nodes.
+        state_function : list
+            Adapted y-values for interpolation containing values of state function.
+        """
+
+        if not left_is_node:
+            t_interp.insert(0, t)
+        else:
+            del state_function[0]
+
+        return t_interp, state_function

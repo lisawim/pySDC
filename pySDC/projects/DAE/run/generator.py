82c0b070a6801e2134d27cd8055e38aa4c748d9b
lisawim <wimmer@uni-wuppertal.de>
2023-08-14 07:43:11 +0200

    Bugfix for IEEE9BusSystem + plot does work now

--- pySDC/projects/DAE/run/generator.py
+++ pySDC/projects/DAE/run/generator.py
@@ -19,13 +19,13 @@ def run():
 
     # initialize level parameters
     level_params = dict()
-    level_params['restol'] = 1e-9
-    level_params['dt'] = 1e-5
+    level_params['restol'] = 1e-12
+    level_params['dt'] = 1e-3
 
     # initialize sweeper parameters
     sweeper_params = dict()
     sweeper_params['quad_type'] = 'RADAU-RIGHT'
-    sweeper_params['num_nodes'] = 4
+    sweeper_params['num_nodes'] = 2
     sweeper_params['QI'] = 'LU'
 
     # initialize problem parameters
@@ -60,7 +60,7 @@ def run():
 
     # set time parameters
     t0 = 0.0
-    Tend = 0.01 #1e-3 #1.0
+    Tend = 2.0 #1e-3 #1.0
 
     # get initial values on finest level
     P = controller.MS[0].levels[0].prob
@@ -78,7 +78,7 @@ def run():
     # V = np.array([me[1][11*m + 2*m:11*m + 2*m + n] for me in get_sorted(stats, type='approx_solution', sortby='time')])
     V = np.array([me[1][11*m + 2*m] for me in get_sorted(stats, type='approx_solution', sortby='time')])
     t = np.array([me[0] for me in get_sorted(stats, type='approx_solution', sortby='time')])
-    print(V)
+    # print([me[1][11*m + 2*m:11*m + 2*m + n] for me in get_sorted(stats, type='approx_solution', sortby='time', recomputed=False)])
     Eqp = np.array([me[1][0:m] for me in get_sorted(stats, type='approx_solution', sortby='time')])
     Si1d = np.array([me[1][m:2*m] for me in get_sorted(stats, type='approx_solution', sortby='time')])
     Edp = np.array([me[1][2*m:3*m] for me in get_sorted(stats, type='approx_solution', sortby='time')])
@@ -95,25 +95,34 @@ def run():
     V = np.array([me[1][11*m + 2*m:11*m + 2*m + n] for me in get_sorted(stats, type='approx_solution', sortby='time')])
     TH = np.array([me[1][11*m + 2*m + n:11*m + 2*m + 2 * n] for me in get_sorted(stats, type='approx_solution', sortby='time')])
 
-    print('Eqp=', Eqp - x0[0:m])
-    print('Si1d=', Si1d - x0[m:2*m])
-    print('Edp=', Edp - x0[2*m:3*m])
-    print('ESi2q=', ESi2q - x0[3*m:4*m])
-    print('Delta=', Delta - x0[4*m:5*m])
-    print('w=', w - x0[5*m:6*m])
-    print('Efd=', Efd - x0[6*m:7*m])
-    print('RF=', RF - x0[7*m:8*m])
-    print('VR=', VR - x0[8*m:9*m])
-    print('TM=', TM - x0[9*m:10*m])
-    print('PSV=', PSV - x0[10*m:11*m])
-    print('Id=', Id - x0[11*m:12*m])
-    print('Iq=', Iq - x0[12*m:13*m])
-    print('V=', V - x0[11*m + 2*m:11*m + 2*m + n])
-    print('TH=', TH - x0[11*m + 2*m + n:11*m + 2*m + 2 * n])
+    # print('Eqp=', Eqp - x0[0:m])
+    # print('Si1d=', Si1d - x0[m:2*m])
+    # print('Edp=', Edp - x0[2*m:3*m])
+    # print('ESi2q=', ESi2q - x0[3*m:4*m])
+    # print('Delta=', Delta - x0[4*m:5*m])
+    # print('w=', w - x0[5*m:6*m])
+    # print('Efd=', Efd - x0[6*m:7*m])
+    # print('RF=', RF - x0[7*m:8*m])
+    # print('VR=', VR - x0[8*m:9*m])
+    # print('TM=', TM - x0[9*m:10*m])
+    # print('PSV=', PSV - x0[10*m:11*m])
+    # print('Id=', Id - x0[11*m:12*m])
+    # print('Iq=', Iq - x0[12*m:13*m])
+    # print('V=', V - x0[11*m + 2*m:11*m + 2*m + n])
+    # print('TH=', TH - x0[11*m + 2*m + n:11*m + 2*m + 2 * n])
 
 
     fig3, ax3 = plt_helper.plt.subplots(1, 1, figsize=(4.5, 3))
-    ax3.plot(t, V, label='V0')
+    ax3.plot(t, V[:, 0], label='V0')
+    ax3.plot(t, V[:, 1], label='V1')
+    ax3.plot(t, V[:, 2], label='V2')
+    ax3.plot(t, V[:, 3], label='V3')
+    ax3.plot(t, V[:, 4], label='V4')
+    ax3.plot(t, V[:, 5], label='V5')
+    ax3.plot(t, V[:, 6], label='V6')
+    ax3.plot(t, V[:, 7], label='V7')
+    ax3.plot(t, V[:, 8], label='V8')
+    ax3.legend(loc='upper right', fontsize=10)
     fig3.savefig('data/V0.png', dpi=300, bbox_inches='tight')
     plt_helper.plt.close(fig3)
 

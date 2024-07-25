41d5a213a7fec7060a1d3ed0b30bc510bcd1865d
lisawim <wimmer@uni-wuppertal.de>
2024-07-25 07:40:22 +0200

    Shorten import paths further by adding another  __init__.py

---
+++ pySDC/projects/DAE/__init__.py
@@ -0,0 +1,14 @@
+from .run import QI_SERIAL, QI_PARALLEL, getEndTime, computeSolution, getColor, getLabel, getMarker, Plotter
+from .misc.denseOutput import DenseOutput
+
+__all__ = [
+    # 'QI_SERIAL',
+    # 'QI_PARALLEL',
+    'getEndTime',
+    'computeSolution',
+    'getColor',
+    'getLabel',
+    'getMarker',
+    'Plotter',
+    'DenseOutput',
+]

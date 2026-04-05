46eee8b86a808eb28815941450fff9fecb6cb2d1
lisawim <wimmer@uni-wuppertal.de>
2026-04-05 17:03:13 +0200

    Added hooks for sweeps, solution and CPU timings.

--- pySDC/projects/DAE/misc/hooksDAE.py
+++ pySDC/projects/DAE/misc/hooksDAE.py
@@ -1,4 +1,250 @@
+import time
 from pySDC.core.hooks import Hooks
+
+
+class LogGlobalErrorPreSweepDiff(Hooks):
+    def pre_sweep(self, step, level_number):
+        super().pre_sweep(step, level_number)
+
+        L = step.levels[level_number]
+        P = L.prob
+
+        # L.sweep.compute_end_point()
+
+        upde = P.u_exact(step.time + step.dt)
+        e_global = abs(upde[:].diff - L.u[-1][:].diff)
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type="e_global_differential_pre_sweep",
+            value=e_global,
+        )
+
+
+class LogGlobalErrorPreSweepAlg(Hooks):
+    def pre_sweep(self, step, level_number):
+        super().pre_sweep(step, level_number)
+
+        L = step.levels[level_number]
+        P = L.prob
+
+        # L.sweep.compute_end_point()
+
+        upde = P.u_exact(step.time + step.dt)
+        e_global_algebraic = abs(upde[:].alg - L.u[-1][:].alg)
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type="e_global_algebraic_pre_sweep",
+            value=e_global_algebraic,
+        )
+
+
+class LogGlobalErrorPostSweepDiff(Hooks):
+    """
+    Hook class to log the global error in the differential variable and its derivative after each sweep.
+    """
+
+    def post_sweep(self, step, level_number):
+        super().post_sweep(step, level_number)
+
+        L = step.levels[level_number]
+        P = L.prob
+
+        L.sweep.compute_end_point()
+
+        upde = P.u_exact(step.time + step.dt)
+        e_global = abs(upde[:].diff - L.uend[:].diff)
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type="e_global_differential_post_sweep",
+            value=e_global,
+        )
+
+
+class LogGlobalErrorPostSweepAlg(Hooks):
+    """
+    Hook class to log the global error in the algebraic variable and its derivative after each sweep.
+    """
+
+    def post_sweep(self, step, level_number):
+        super().post_sweep(step, level_number)
+
+        L = step.levels[level_number]
+        P = L.prob
+
+        L.sweep.compute_end_point()
+
+        upde = P.u_exact(step.time + step.dt)
+        e_global_algebraic = abs(upde[:].alg - L.uend[:].alg)
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type="e_global_algebraic_post_sweep",
+            value=e_global_algebraic,
+        )
+
+
+class LogExactError(Hooks):
+    """
+    Store the exact error from convergence controller at the end of each step as "exact_error".
+    """
+
+    def post_step(self, step, level_number, appendix=''):
+        """
+        Record exact error.
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+
+        Returns:
+            None
+        """
+        super().post_step(step, level_number)
+
+        L = step.levels[level_number]
+
+        self.add_to_stats(
+            process=step.status.slot,
+            process_sweeper=L.sweep.rank,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=iter,
+            sweep=L.status.sweep,
+            type="exact_error",
+            value=L.status.exact_error,
+        )
+
+
+class MyCPUTimings(Hooks):
+    """Logs iteration timing at L.time + L.dt instead of L.time."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.__t0_iteration = None
+        self.__t0_step = None
+        self.__t1_iteration = None
+        self.__t1_step = None
+
+    def _compute_time_elapsed(self, event_after, event_before):
+        return event_after - event_before
+
+    def _get_event(self):
+        return time.perf_counter()
+    
+    def pre_step(self, step, level_number):
+        """
+        Hook called before each step
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+        """
+        super().pre_step(step, level_number)
+        self.__t0_step = self._get_event()
+
+    def pre_iteration(self, step, level_number):
+        """
+        Default routine called before iteration starts
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+        """
+        super().pre_iteration(step, level_number)
+        self.__t0_iteration = self._get_event()
+
+    def post_iteration(self, step, level_number):
+        super().post_iteration(step, level_number)
+        self.__t1_iteration = self._get_event()
+
+        L = step.levels[level_number]
+
+        self.add_to_stats(
+            process=step.status.slot,
+            process_sweeper=L.sweep.rank,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type=f"timing_post_iteration",
+            value=self._compute_time_elapsed(self.__t1_iteration, self.__t0_iteration),
+        )
+
+    def post_step(self, step, level_number):
+        """
+        Default routine called after each step or block
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+        """
+        super().post_step(step, level_number)
+        self.__t1_step = self._get_event()
+
+        L = step.levels[level_number]
+
+        self.add_to_stats(
+            process=step.status.slot,
+            process_sweeper=L.sweep.rank,
+            time=L.time,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type=f"timing_post_step",
+            value=self._compute_time_elapsed(self.__t1_step, self.__t0_step),
+        )
+
+
+class MyLogSolutionAfterIteration(Hooks):
+    """
+    Store the solution at the end of each iteration as "u_post_iteration".
+    """
+
+    def post_iteration(self, step, level_number):
+        """
+        Record solution at the end of the iteration
+
+        Args:
+            step (pySDC.Step.step): the current step
+            level_number (int): the current level number
+
+        Returns:
+            None
+        """
+        super().post_iteration(step, level_number)
+
+        L = step.levels[level_number]
+        L.sweep.compute_end_point()
+
+        self.add_to_stats(
+            process=step.status.slot,
+            time=L.time + L.dt,
+            level=L.level_index,
+            iter=step.status.iter,
+            sweep=L.status.sweep,
+            type="u_post_iteration",
+            value=L.uend,
+        )
 
 
 class LogAbsValuePreIterAlgebraicConstraints(Hooks):

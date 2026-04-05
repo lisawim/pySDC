962cff63b9fcf9eeaa3dbb6ce03b36d9e5b26d38
lisawim <wimmer@uni-wuppertal.de>
2026-04-05 17:09:15 +0200

    Update configs.

--- pySDC/projects/DAE/misc/configurations.py
+++ pySDC/projects/DAE/misc/configurations.py
@@ -1,63 +1,95 @@
-from pySDC.implementations.hooks.log_errors import LogGlobalErrorPostStep
+from pySDC.implementations.hooks.log_errors import LogGlobalErrorPostIter, LogGlobalErrorPostStep
 from pySDC.implementations.hooks.log_embedded_error_estimate import LogEmbeddedErrorEstimate
 from pySDC.implementations.hooks.log_solution import LogSolution
+from pySDC.projects.DAE.misc.hooksDAE import LogExactError, MyCPUTimings, MyLogSolutionAfterIteration
+from pySDC.projects.DAE.problems.andrewsSqueezingMechanism import LogPositionErrorEndPostIteration
 from pySDC.projects.DAE.run.plot_order_iteration import choose_time_step_sizes
 
 
-def get_configs(problem_name: str, config_type: str) -> dict:
+def _ensure_known_problem(problem_name: str) -> None:
+    if problem_name not in {"ANDREWS-SQUEEZER", "LINEAR-TEST", "REACTION-DIFFUSION"}:
+        raise NotImplementedError(f"Unknown/unsupported problem_name={problem_name!r}")
+
+
+def get_configs(problem_name: str, config_type: str, nsweeps: int = None) -> dict:
+    _ensure_known_problem(problem_name)
+
     if config_type == "work_precision":
-        hook_class = [LogGlobalErrorPostStep]
         num_nodes = 6
-        sweepers = ["constrainedDAE", "semiImplicitDAE"]
-        test_methods = ["EE", "IE", "LU", "MIN-SR-NS", "MIN-SR-S", "Picard", "DOPRI5", "RadauIIA5", "RadauIIA7"]
-
-        if problem_name in ["ANDREWS-SQUEEZER", "LINEAR-TEST"]:
-            test_methods = ["EE", "IE", "LU", "MIN-SR-NS", "MIN-SR-S", "Picard", "DOPRI5", "RadauIIA5", "RadauIIA7"]
-        elif problem_name == "REACTION-DIFFUSION":
-            # Only implicit methods since problem is stiff
-            test_methods = ["IE", "LU", "MIN-SR-S", "RadauIIA5", "RadauIIA7"]
-        else:
-            raise NotImplementedError(f"Test methods for {problem_name} are not implemented!")
-
         config = {
-            "hook_class": hook_class,
-            "num_nodes": num_nodes,
+            "hook_class": [LogGlobalErrorPostStep],
+            "num_nodes": 6,
+            "nsweeps": nsweeps if nsweeps is not None else num_nodes,
             "problem_name": problem_name,
-            "sweepers": sweepers,
-            "test_methods": test_methods,
+            "sweepers": ["constrainedDAE", "semiImplicitDAE"],
+            "test_methods": [
+                "EE",
+                "IE",
+                "LU",
+                "MIN-SR-NS",
+                "MIN-SR-S",
+                "MIN-SR-FLEX",
+                "Picard",
+                "DOPRI5",
+                "RadauIIA5",
+                "RadauIIA7",
+            ],
         }
+        assert config["nsweeps"] <= num_nodes, "nsweeps must be <= num_nodes for work-precision config"
+
+        # Only implicit methods since problem is stiff
+        if problem_name == "REACTION-DIFFUSION":
+            config["test_methods"] = ["IE", "LU", "MIN-SR-S", "RadauIIA5", "RadauIIA7"]
 
     elif config_type == "scaling":
-        QI_serial_methods = ["LU", "DOPRI5", "RadauIIA5", "RadauIIA7"]
-        QI_parallel_methods = ["MIN-SR-NS", "MIN-SR-S"]
-
-        sweepers = ["constrainedDAE", "semiImplicitDAE"]
         dt_list, _ = choose_time_step_sizes(problem_name=problem_name)
 
-        hook_class = [LogEmbeddedErrorEstimate, LogGlobalErrorPostStep, LogSolution]
+        config = {
+            "hook_class": [
+                MyCPUTimings,
+                LogEmbeddedErrorEstimate,
+                LogGlobalErrorPostIter,
+                LogGlobalErrorPostStep,
+                LogSolution,
+                MyLogSolutionAfterIteration,
+            ],
+            "problem_name": problem_name,
+            "sweepers": ["constrainedDAE", "semiImplicitDAE"],
+            "QI_serial_methods": ["LU", "DOPRI5", "RadauIIA5", "RadauIIA7"],
+            "QI_parallel_methods": ["MIN-SR-NS", "MIN-SR-S"],
+            "dt": None,
+        }
+
+        # problem-specific overrides
+        if problem_name == "ANDREWS-SQUEEZER":
+            config["hook_class"] += [LogPositionErrorEndPostIteration]
 
         if problem_name == "REACTION-DIFFUSION":
-            QI_serial_methods = ["LU", "RadauIIA5", "RadauIIA7"]
-            QI_parallel_methods = ["MIN-SR-S"]
+            config["QI_serial_methods"] = ["LU", "RadauIIA5", "RadauIIA7"]
+            config["QI_parallel_methods"] = ["MIN-SR-S"]
 
-        if problem_name == "ANDREWS-SQUEEZER":
-            dt = dt_list[0]
-        elif problem_name == "LINEAR-TEST":
-            dt = dt_list[3]
-        elif problem_name == "REACTION-DIFFUSION":
-            dt = dt_list[2]
-
-        config = {
-            "hook_class": hook_class,
-            "problem_name": problem_name,
-            "dt": dt,
-            "sweepers": sweepers,
-            "QI_serial_methods": QI_serial_methods,
-            "QI_parallel_methods": QI_parallel_methods,
+        dt_index_by_problem = {
+            "ANDREWS-SQUEEZER": 0,
+            "LINEAR-TEST": 3,
+            "REACTION-DIFFUSION": 1,
         }
+        config["dt"] = dt_list[dt_index_by_problem[problem_name]]
+
+    elif config_type == "speedup_at_accuracy":
+        config = get_configs(problem_name=problem_name, config_type="scaling")
+        config["hook_class"] = [
+            MyCPUTimings, LogEmbeddedErrorEstimate, LogExactError, LogGlobalErrorPostStep
+        ]
+        config["stop_at_accuracy_for_speedup"] = True
 
     elif config_type == "breakeven":
         config = get_configs(problem_name=problem_name, config_type="scaling")
         config.pop("QI_serial_methods", None)
+
+        if problem_name in ["ANDREWS-SQUEEZER", "LINEAR-TEST"]:
+            config["QI_parallel_methods"] = ["MIN-SR-NS"]
+
+    else:
+        raise NotImplementedError(f"Unknown config_type={config_type!r}")
 
     return config

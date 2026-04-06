127e6ec888c300483c8c8b587e80d55054b4e502
lisawim <wimmer@uni-wuppertal.de>
2026-04-06 17:55:40 +0200

    Added script to generate plots for my thesis.

---
+++ pySDC/projects/DAE/run/thesis_plots.py
@@ -0,0 +1,32 @@
+def make_plots_for_chapter_application(journal="BUW_thesis"):
+    from pySDC.projects.DAE.run.solution import (
+        plot_solution_piline,
+        plot_solution_buck_converter,
+        plot_solution_battery,
+    )
+
+    plot_solution_piline(filename="piline_solution", journal=journal)
+    plot_solution_buck_converter(filename="buck_converter_solution", journal=journal)
+    plot_solution_battery(filename="battery_solution", journal=journal)
+
+def make_plots_for_chapter_test_problems(journal="BUW_thesis"):
+    from pySDC.projects.DAE.run.solution import (
+        plot_solution_linear,
+        plot_solution_linear_embedded,
+        plot_solution_andrews,
+        plot_solution_reaction_diffusion,
+        plot_solution_discontinuous_test,
+        plot_solution_wscc9,
+    )
+
+    plot_solution_linear(filename="linear_test_solution", journal=journal)
+    plot_solution_linear_embedded(filename="linear_test_embedded_solution", journal=journal)
+    plot_solution_andrews(filename="andrews_squeezer_solution", journal=journal)
+    plot_solution_reaction_diffusion(filename="reaction_diffusion_solution", journal=journal)
+    plot_solution_discontinuous_test(filename="discontinuous_test_solution", journal=journal)
+    plot_solution_wscc9(filename="wscc9_solution", journal=journal)
+
+
+if __name__ == "__main__":
+    make_plots_for_chapter_application()
+    # make_plots_for_chapter_test_problems()

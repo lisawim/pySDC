5fa8a211dfad1c4ad75cad90c33d4a1020556e10
lisawim <wimmer@uni-wuppertal.de>
2026-05-10 09:14:15 +0200

    Added imex DAE mesh.

--- pySDC/projects/DAE/misc/meshDAE.py
+++ pySDC/projects/DAE/misc/meshDAE.py
@@ -1,4 +1,4 @@
-from pySDC.implementations.datatype_classes.mesh import MultiComponentMesh
+from pySDC.implementations.datatype_classes.mesh import MultiComponentMesh, imex_mesh
 
 
 class MeshDAE(MultiComponentMesh):
@@ -10,3 +10,42 @@ class MeshDAE(MultiComponentMesh):
     """
 
     components = ['diff', 'alg']
+
+
+class imex_dae_mesh(MultiComponentMesh):
+    r"""
+    Datatype for IMEX DAE problems.
+
+    The differential part is split into an implicit and an explicit
+    contribution, while the algebraic part is stored separately.
+
+    Internally, the storage is flat:
+
+        u.diff_impl
+        u.diff_expl
+        u.alg
+
+    For convenience, the differential part can also be accessed as
+
+        u.diff.impl
+        u.diff.expl
+    """
+
+    components = ["diff_impl", "diff_expl", "alg"]
+
+    @property
+    def diff(self):
+        r"""
+        Differential part of the IMEX DAE.
+
+        Returns a view with components
+
+            diff.impl
+            diff.expl
+
+        corresponding to
+
+            self.diff_impl
+            self.diff_expl
+        """
+        return self[:2].view(imex_mesh)

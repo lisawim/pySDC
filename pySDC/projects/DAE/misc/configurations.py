ac20c09f9ebf5d08e8544ec211414c4284277d71
lisawim <wimmer@uni-wuppertal.de>
2026-05-12 12:58:43 +0200

    Added configs for scaling and speedup.

--- pySDC/projects/DAE/misc/configurations.py
+++ pySDC/projects/DAE/misc/configurations.py
@@ -4,6 +4,7 @@ from pySDC.implementations.hooks.log_errors import (
     LogGlobalErrorPostStep, LogGlobalErrorPostIter, LogGlobalErrorPostSweep
 )
 from pySDC.implementations.hooks.log_embedded_error_estimate import LogEmbeddedErrorEstimate
+from pySDC.projects.DAE.misc.log_exact_error import LogExactError
 from pySDC.projects.DAE.problems.andrewsSqueezingMechanism import LogPositionErrorEnd
 from pySDC.implementations.hooks.log_solution import LogSolution
 from pySDC.projects.DAE.misc.log_solution import MyLogSolutionAfterIteration
@@ -83,6 +84,15 @@ def get_configs(problem_name: str, config_type: str) -> dict:
             "REACTION-DIFFUSION": 1,
         }
         config["dt"] = dt_list[dt_index_by_problem[problem_name]]
+    elif config_type == "speedup_at_accuracy":
+        config = get_configs(problem_name=problem_name, config_type="scaling")
+        config["hook_class"] = [
+            MyCPUTimings, LogEmbeddedErrorEstimate, LogExactError, LogGlobalErrorPostStep
+        ]
+        # if problem_name == "REACTION-DIFFUSION":
+        #     config["hook_class"] += [LogAchievedNewtonTolerancePostStep]
+
+        config["stop_at_accuracy_for_speedup"] = True
     else:
         raise NotImplementedError(f"Unknown config_type={config_type!r}")
 

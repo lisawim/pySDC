d44048cc422d0a049851e6e4dbb8f41cb179e710
lisawim <wimmer@uni-wuppertal.de>
2026-06-18 09:57:25 +0200

    Changed filenames in paper2_plots script.

--- pySDC/projects/DAE/run/paper2_plots.py
+++ pySDC/projects/DAE/run/paper2_plots.py
@@ -9,7 +9,7 @@ def make_scaling_plots(global_comm, journal="SIAM_Scientific_Computing"):
     # Plots for LINEAR-TEST
     print("\nGenerating scaling plots for LINEAR-TEST...\n")
     config_linear = get_configs(problem_name="LINEAR-TEST", config_type="scaling")
-    filename = "results_scaling_dt=0.05_linear_#7.pkl"
+    filename = "results_scaling_dt=0.05_linear.pkl"
     plots_scaling(
         global_comm=global_comm, format="png", filename=filename, journal=journal, ref_QI="IE", **config_linear
     )
@@ -17,7 +17,7 @@ def make_scaling_plots(global_comm, journal="SIAM_Scientific_Computing"):
     # Plots for ANDREWS-SQUEEZER
     print("\nGenerating scaling plots for ANDREWS-SQUEEZER...\n")
     config_andrews = get_configs(problem_name="ANDREWS-SQUEEZER", config_type="scaling")
-    filename = "results_scaling_dt=0.001_andrews_#9.pkl"
+    filename = "results_scaling_dt=0.001_andrews.pkl"
     nodes_to_plot = range(2, 17)
     plots_scaling(
         global_comm=global_comm,
@@ -32,7 +32,7 @@ def make_scaling_plots(global_comm, journal="SIAM_Scientific_Computing"):
     # Plots for REACTION-DIFFUSION
     print("\nGenerating scaling plots for REACTION-DIFFUSION...\n")
     config_reacdiff = get_configs(problem_name="REACTION-DIFFUSION", config_type="scaling")
-    filename = "results_scaling_dt=0.05_reaction_diffusion_#14.pkl"
+    filename = "results_scaling_dt=0.05_reaction_diffusion.pkl"
     plots_scaling(
         global_comm=global_comm, format="png", filename=filename, journal=journal, ref_QI="IE", **config_reacdiff
     )
@@ -44,15 +44,14 @@ def make_speedup_at_accuracy_plots(global_comm, journal="SIAM_Scientific_Computi
     # Plots for LINEAR-TEST
     print("\nGenerating speedup plots for LINEAR-TEST...\n")
     config_linear = get_configs(problem_name="LINEAR-TEST", config_type="speedup_at_accuracy")
-    filename = "results_speedup_at_acc_dt=0.05_linear_#5.pkl"
+    filename = "results_speedup_at_acc_dt=0.05_linear.pkl"
     plots_speedup_at_accuracy(
         global_comm=global_comm, filename=filename, journal=journal, ref_QI="IE", **config_linear
     )
 
     print("\nGenerating speedup plots for REACTION-DIFFUSION...\n")
     config_reacdiff = get_configs(problem_name="REACTION-DIFFUSION", config_type="speedup_at_accuracy")
-    # filename = "results_speedup_at_acc_dt=0.05_reaction_diffusion_#2.pkl"
-    filename = "results_speedup_at_acc_dt=0.05_reaction_diffusion_#12.pkl"
+    filename = "results_speedup_at_acc_dt=0.05_reaction_diffusion.pkl"
     plots_speedup_at_accuracy(
         global_comm=global_comm, filename=filename, journal=journal, ref_QI="IE", **config_reacdiff
     )
